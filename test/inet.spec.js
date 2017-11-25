e5e88fa90f49f09c304841b2060d775b49f90fcb
mkg20001 <mkg20001@gmail.com>
2017-11-25 19:52:46 +0100

    feat: long2ip, ip2long

--- test/inet.spec.js
+++ test/inet.spec.js
@@ -2,7 +2,7 @@
 
 'use strict'
 
-const assert = require('assert')
+const assert = require('assert').equal
 const inet = require('../src')
 
 describe('inet-tools', () => {
@@ -15,4 +15,12 @@ describe('inet-tools', () => {
 
   it('ntop #1', () => assert(inet.ntop('\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x01'), '::1'))
   it('ntop #2', () => assert(inet.ntop('\x7F\x00\x00\x01'), '127.0.0.1'))
+
+  it('ip2long #1', () => assert(inet.ip2long('192.0.34.166'), 3221234342))
+  it('ip2long #2', () => assert(inet.ip2long('0.0xABCDEF'), 11259375))
+  it('ip2long #3', () => assert(inet.ip2long('0.171.205.239'), 11259375))
+  it('ip2long #4', () => assert(inet.ip2long('255.255.255.256'), false))
+
+  it('long2ip #1', () => assert(inet.long2ip(3221234342), '192.0.34.166'))
+  it('long2ip #2', () => assert(inet.long2ip(11259375), '0.171.205.239'))
 })
